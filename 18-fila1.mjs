b70bce60049aa73f2fe310c27e65b2dd2c3c92c6
AndreWar10 <Andreguerra2002@hotmail.com>
2021-05-04 01:25:19 +0000

    (03-05)- Introdução a fila

---
+++ 18-fila1.mjs
@@ -0,0 +1,28 @@
+import {Queue} from './lib/Queue2.mjs'
+
+let fila = new Queue()
+console.log(fila.print())
+
+fila.enqueue('Creusdete')
+console.log(fila.print())
+
+fila.enqueue('Jubinaldo')
+console.log(fila.print())
+
+fila.enqueue('Neurivânia')
+console.log(fila.print())
+
+//Não tira pessoa da fila. Só visualiza
+let proximo = fila.peek()
+console.log({proximo})
+console.log(fila.print())
+
+//Tirar pessoa da fila
+let atendido = fila.dequeue()
+console.log({atendido})
+console.log(fila.print())
+
+//Colocar pessoa na fila
+fila.enqueue('Leniwander')
+console.log(fila.print())
+
